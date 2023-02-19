31dbdceabe8b8ba3f5434abd9412b23a7a84e8bb
Alonso Quispe <alonsodaniel619@gmail.com>
2023-02-18 22:36:15 -0500

    segundo commit

--- src/main/java/com/mitocode/controller/PatientController.java
+++ src/main/java/com/mitocode/controller/PatientController.java
@@ -1,21 +1,62 @@
 package com.mitocode.controller;
 
 import com.mitocode.model.Patient;
-import com.mitocode.service.PatientService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.mitocode.service.IPatientService;
+import com.mitocode.service.impl.PatientServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/patients")
 public class PatientController {
 
-    private PatientService service = new PatientService();
+    @Autowired
+    private IPatientService service;
 
     @GetMapping
-    public String sayHello() {
-        Patient patient = new Patient(1,"Mitocode");
-        return service.sayHello(patient);
+    public ResponseEntity<List<Patient>> findAll() {
+        List<Patient> list = service.findAll();
+        return new ResponseEntity<>(list, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Patient> findById(@PathVariable("id") Integer id) {
+        Patient obj = service.findById(id);
+        return new ResponseEntity<>(obj, HttpStatus.OK);
+    }
+
+    /*@PostMapping
+    public ResponseEntity<Patient> save(@RequestBody Patient patient) {
+        Patient obj = service.save(patient);
+        return new ResponseEntity<>(obj, HttpStatus.CREATED);
+    }*/
+
+    @PostMapping
+    public ResponseEntity<Void> save(@RequestBody Patient patient) {
+        Patient obj = service.save(patient);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(obj.getIdPatient())
+                .toUri();
+        return ResponseEntity.created(location).build();
+    }
+
+    @PutMapping
+    public ResponseEntity<Patient> update(@RequestBody Patient patient) {
+        Patient obj = service.update(patient);
+        return new ResponseEntity<>(obj, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable("id") Integer id) {
+        service.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 }
--- src/main/java/com/mitocode/model/Patient.java
+++ src/main/java/com/mitocode/model/Patient.java
@@ -1,18 +1,38 @@
 package com.mitocode.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+
+import javax.persistence.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
+@Entity
 public class Patient {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idPatient;
-    private String name;
+
+    @Column(length = 70, nullable = false)
+    private String firstName;
+
+    @Column(length = 70, nullable = false)
+    private String lastName;
+
+    @Column(length = 8, nullable = false)
+    private String dni;
+
+    @Column(length = 150, nullable = false)
+    private String address;
+
+    @Column(length = 9, nullable = false)
+    private String phone;
+
+    @Column(length = 55, nullable = false)
+    private String email;
 
 
 }
